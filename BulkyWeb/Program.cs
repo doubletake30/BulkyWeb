b5d017de48c43936ca13d3ef7c1d3a8667c6efbe
doubletake30 <86301855+doubletake30@users.noreply.github.com>
2024-05-29 13:20:40 +0700

    feat: Create table, controller, view of Category

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using BulkyWeb.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
